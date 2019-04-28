6ae471c5db7299a67dbb9f5e73b714ac17f9450a
taigacute <Marlboro@astrobss.com>
2019-04-28 15:36:03 +0800

    update

--- install/install.sh
+++ install/install.sh
@@ -5,6 +5,9 @@ brew tap crisidev/homebrew-chunkwm
 brew install --HEAD --with-tmp-logging chunkwm
 brew install --HEAD --with-logging  koekeishiya/formulae/skhd
 
+# tmux plugin urlview
+brew install urlview
+brew install extract_url
 
 # start service
 brew services start chunkwm
