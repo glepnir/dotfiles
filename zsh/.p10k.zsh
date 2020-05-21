44eff09b09d58e7906b7baa92801209ab6fac107
glepnir <glephunter@gmail.com>
2020-05-21 13:22:22 +0800

    update

--- zsh/.p10k.zsh
+++ zsh/.p10k.zsh
@@ -90,15 +90,15 @@
     nix_shell               # nix shell (https://nixos.org/nixos/nix-pills/developing-with-nix-shell.html)
     # vpn_ip                # virtual private network indicator
     # load                  # CPU load
-    disk_usage            # disk usage
-    ram                   # free RAM
-    swap                  # used swap
+    # swap                  # used swap
     todo                    # todo items (https://github.com/todotxt/todo.txt-cli)
     timewarrior             # timewarrior tracking status (https://timewarrior.net/)
     taskwarrior             # taskwarrior task count (https://taskwarrior.org/)
+    time                    # current time
     # =========================[ Line #2 ]=========================
     newline
-    time                    # current time
+    disk_usage            # disk usage
+    ram                   # free RAM
     # ip                    # ip address and bandwidth usage for a specified network interface
     # public_ip             # public IP address
     # proxy                 # system-wide http/https/ftp proxy
