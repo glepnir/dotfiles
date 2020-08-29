dd7d42898a78e1655e982d9ca391f91497052d05
Raphael <glephunter@gmail.com>
2020-08-30 00:03:30 +0800

    update

--- config/nvim/lua/vim_map.lua
+++ config/nvim/lua/vim_map.lua
@@ -1,40 +1,31 @@
 require 'global'
 
-vim_map = {}
+vim_map = {
+  normal  = {},
+  insert  = {},
+  command = {},
+}
 
-function vim_map.load_vim_map()
-  local mappings = {
-    -- normal map
-    ["nY"]         = {"y$",noremap = true},
-    ["n<C-x>k"]    = map_call('BD'),
-    ["n<C-s>"]     = map_cmd('write'),
-    ["n]w"]        = map_cmd("WhitespaceNext"),
-    ["n[w"]        = map_cmd("WhitespacePrev"),
-    -- ["n<Space>cw"] = map_cmd("keeppatterns %substitute/\s\+$//e"),
-
-    -- ["i<C-w>"] = {"<C-[>diwa",noremap = true},
-    -- ["i<C-h>"] = {"<BS>",noremap = true},
-    -- ["i<C-d>"] = {"<Del>",noremap =true},
-    -- ["i<C-k>"] = {"<ESC>d$a"},
-
-    -- ["i<C-u>"]       = {"<C-G>u<C-U>",noremap =true},
-    -- ["i<C-b>"]       = {"<Left>",noremap =true},
-    -- ["i<C-f>"]       = {"<Right>",noremap = true },
-    -- ["i<C-a>"]       = {"<ESC>^i",noremap = true},
-    -- ["i<expr><C-e>"] = [[pumvisible() ? "\<C-e>" : "\<End>"]],
-    -- ["i<C-O>"]       = {"<Esc>o",noremap =true},
-    -- ["i<C-S>"]        = {"<esc>:w<CR>"},
-    -- ["i<C-Q>"]        = {"<esc>:wq<CR>"},
-    --
-    -- ["c<C-p>"] = {"<Up>",noremap = true},
-    -- ["c<C-b>"] = {"<Left>",noremap = true},
-    -- ["c<C-f>"] = {"<Right>", noremap = true},
-    -- ["c<C-a>"] = {"<Home>", noremap = true},
-    -- ["c<C-e>"] = {"<End>", noremap = true},
-    -- ["c<C-d>"] = {"<Del>", noremap =true},
-    -- ["c<C-h>"] = {"<BS>", noremap = true},
-    -- ["c<C-t>"] = [[<C-R>=expand("%:p:h") . "/" <CR>]],
-  }
-
-  nvim_apply_mappings(mappings, {silent = true})
+function vim_map:create()
+  instance = {}
+  setmetatable(instance, self)
+  self.__index = self
+  return instance
 end
+
+function vim_map:define()
+  self.normal = {
+    ["<C-x>k"] = "BD",
+    ["<C-s>"] = "write"
+  }
+end
+
+function nvim_define_map(items)
+  for k,v in pairs(items.normal) do
+    vim.api.nvim_set_keymap('n')
+  end
+end
+
+
+ins = vim_map:create()
+ins:define()
