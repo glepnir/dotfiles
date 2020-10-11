e15e61ee937c84f92495fed9e762e35684bc874e
Raphael <glephunter@gmail.com>
2020-10-11 16:27:31 +0800

    update

--- zsh/.p10k-cloud.zsh
+++ zsh/.p10k-cloud.zsh
@@ -329,7 +329,7 @@
 
   #####################################[ vcs: git status ]######################################
   # Branch icon. Set this parameter to '\uF126 ' for the popular Powerline branch icon.
-  typeset -g POWERLEVEL9K_VCS_BRANCH_ICON=' '
+  typeset -g POWERLEVEL9K_VCS_BRANCH_ICON=' '
 
   # Untracked files icon. It's really a question mark, your font isn't broken.
   # Change the value of this parameter to show a different icon.
