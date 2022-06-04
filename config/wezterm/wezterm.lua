59049f7fa9f444cafe29d699f031f81bda6d70ff
Raphael <glephunter@gmail.com>
2022-06-04 20:42:09 +0800

    update

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -5,6 +5,7 @@ return {
   font = wt.font('Operator Mono Lig',{weight = 325}),
   font_size = 17,
   color_scheme = "Andromeda",
+  custom_block_glyphs = true,
   window_decorations = "RESIZE",
   window_close_confirmation = "NeverPrompt",
 }
