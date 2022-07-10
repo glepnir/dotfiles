6dd3b40412cd1cd5cf257ac8bef768fe66aabf4c
Raphael <glephunter@gmail.com>
2022-07-10 09:23:25 +0800

    update

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,11 +1,11 @@
-local wt = require('wezterm')
+local wt = require("wezterm")
 
 return {
-  enable_tab_bar = false,
-  font = wt.font('Operator Mono Lig',{weight = 325}),
-  font_size = 17,
-  color_scheme = "Andromeda",
-  custom_block_glyphs = true,
-  window_decorations = "RESIZE",
-  window_close_confirmation = "NeverPrompt",
+	enable_tab_bar = false,
+	font = wt.font("Operator Mono SSm Lig", { weight = 325 }),
+	font_size = 16,
+	color_scheme = "Andromeda",
+	custom_block_glyphs = true,
+	window_decorations = "RESIZE",
+	window_close_confirmation = "NeverPrompt",
 }
