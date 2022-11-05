9ecfe32839494d20d183e846d793e34c4facd68a
Raphael <glephunter@gmail.com>
2022-11-05 20:39:14 +0800

    update

--- zsh/.glepnir-p10k.zsh
+++ zsh/.glepnir-p10k.zsh
@@ -99,8 +99,8 @@
     # ram                   # free RAM
     # swap                  # used swap
     # todo                    # todo items (https://github.com/todotxt/todo.txt-cli)
-    timewarrior             # timewarrior tracking status (https://timewarrior.net/)
-    taskwarrior             # taskwarrior task count (https://taskwarrior.org/)
+    # timewarrior             # timewarrior tracking status (https://timewarrior.net/)
+    # taskwarrior             # taskwarrior task count (https://taskwarrior.org/)
     # time                    # current time
     # =========================[ Line #2 ]=========================
     newline
