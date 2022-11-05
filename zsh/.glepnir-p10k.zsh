4cab084e044ea1a4c34278a61853db3910b93d82
Raphael <glephunter@gmail.com>
2022-11-05 19:02:59 +0800

    update

--- zsh/.glepnir-p10k.zsh
+++ zsh/.glepnir-p10k.zsh
@@ -214,7 +214,7 @@
   # prefix. The shortened directory can be tab-completed to the original.
   # typeset -g POWERLEVEL9K_SHORTEN_STRATEGY=truncate_to_unique
   # Replace removed segment suffixes with this symbol.
-  typeset -g POWERLEVEL9K_SHORTEN_DELIMITER='🍁'
+  # typeset -g POWERLEVEL9K_SHORTEN_DELIMITER='🍁'
   # Color of the shortened directory segments.
   typeset -g POWERLEVEL9K_DIR_SHORTENED_FOREGROUND=103
   # Color of the anchor directory segments. Anchor segments are never shortened. The first
