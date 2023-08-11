270229b2d51a91ab32d38d17fdb53559fbed38f7
mathew <glephunter@gmail.com>
2023-08-11 15:32:24 +0800

    update

--- install.sh
+++ install.sh
@@ -77,115 +77,25 @@ brew doctor
 action "install the latest version of git"
 require_brew git
 
-bot "OK, now I am going to update the .gitconfig for your user info:"
-
-gitfile="$HOME/.gitconfig"
-running "link .gitconfig"
-if [ ! -f "gitfile" ]; then
-  read -r -p "Seems like your gitconfig file exist,do you want delete it? [y|N] " response
-  if [[ $response =~ (y|yes|Y) ]]; then
-    rm -rf $HOME/.gitconfig
-    action "cp ./git/.gitconfig ~/.gitconfig"
-    sudo cp $HOME/.dotfiles/git/.gitconfig  $HOME/.gitconfig
-    ln -s $HOME/.dotfiles/git/.gitignore  $HOME/.gitignore
-    ok
-  else
-    ok "skipped"
-  fi
-fi
-grep 'user = GITHUBUSER'  $HOME/.gitconfig > /dev/null 2>&1
-if [[ $? = 0 ]]; then
-    read -r -p "What is your git username? " githubuser
-
-  fullname=`osascript -e "long user name of (system info)"`
-
-  if [[ -n "$fullname" ]];then
-    lastname=$(echo $fullname | awk '{print $2}');
-    firstname=$(echo $fullname | awk '{print $1}');
-  fi
-
-  if [[ -z $lastname ]]; then
-    lastname=`dscl . -read /Users/$(whoami) | grep LastName | sed "s/LastName: //"`
-  fi
-  if [[ -z $firstname ]]; then
-    firstname=`dscl . -read /Users/$(whoami) | grep FirstName | sed "s/FirstName: //"`
-  fi
-  email=`dscl . -read /Users/$(whoami)  | grep EMailAddress | sed "s/EMailAddress: //"`
-
-  if [[ ! "$firstname" ]]; then
-    response='n'
-  else
-    echo  "I see that your full name is $COL_YELLOW$firstname $lastname$COL_RESET"
-    read -r -p "Is this correct? [Y|n] " response
-  fi
-
-  if [[ $response =~ ^(no|n|N) ]]; then
-    read -r -p "What is your first name? " firstname
-    read -r -p "What is your last name? " lastname
-  fi
-  fullname="$firstname $lastname"
-
-  bot "Great $fullname, "
-
-  if [[ ! $email ]]; then
-    response='n'
-  else
-    echo  "The best I can make out, your email address is $COL_YELLOW$email$COL_RESET"
-    read -r -p "Is this correct? [Y|n] " response
-  fi
-
-  if [[ $response =~ ^(no|n|N) ]]; then
-    read -r -p "What is your email? " email
-    if [[ ! $email ]];then
-      error "you must provide an email to configure .gitconfig"
-      exit 1
-    fi
-  fi
-
-
-  running "replacing items in .gitconfig with your info ($COL_YELLOW$fullname, $email, $githubuser$COL_RESET)"
-
-  # test if gnu-sed or MacOS sed
-
-  sed -i "s/GITHUBFULLNAME/$firstname $lastname/" ./git/.gitconfig > /dev/null 2>&1 | true
-  if [[ ${PIPESTATUS[0]} != 0 ]]; then
-    echo
-    running "looks like you are using MacOS sed rather than gnu-sed, accommodating"
-    sed -i '' "s/GITHUBFULLNAME/$firstname $lastname/"  $HOME/.gitconfig
-    sed -i '' 's/GITHUBEMAIL/'$email'/'  $HOME/.gitconfig
-    sed -i '' 's/GITHUBUSER/'$githubuser'/'  $HOME/.gitconfig
-    ok
-  else
-    echo
-    bot "looks like you are already using gnu-sed. woot!"
-    sed -i 's/GITHUBEMAIL/'$email'/'  $HOME/.gitconfig
-    sed -i 's/GITHUBUSER/'$githubuser'/'  $HOME/.gitconfig
-  fi
-fi
-
 # ###########################################################
 bot "zsh setup"
 # ###########################################################
 
 require_brew zsh
 
-# symslink zsh config
-ZSHRC="$HOME/.zshrc"
-running "Configuring zsh"
-if [ ! -f "ZSHRC" ]; then
-  read -r -p "Seems like your zshrc file exist,do you want delete it? [y|N] " response
-  if [[ $response =~ (y|yes|Y) ]]; then
-    rm -rf $HOME/.zshrc
-    rm -rf $HOME/.zshenv
-    action "link zsh/.zshrc and zsh/.zshenv"
-    ln -s  $HOME/.dotfiles/zsh/zshenv $HOME/.zshenv
-    ln -s  $HOME/.dotfiles/zsh/zshrc $HOME/.zshrc
-    ln -s  $HOME/.dotfiles/zsh/p10k.zsh $HOME/.p10k.zsh
-    source ~/.zshrc
-  else
-    ok "skipped"
+shopt -s dotglob
+for file in "$HOME/.dotfiles/link"/*; do
+  if [ -f "$file" ]; then
+    filename=$(basename "$file")
+    ln -s "$file" "$HOME/$filename"
+    echo "Created symlink: $HOME/$filename -> $file"
   fi
-fi
+done
+shopt -u dotglob
+
+git config --global user.name "glepnir"
+git config --global user.email "glephunter@gmail.com"
+git config --global core.editor "nvim"
 
 # ###########################################################
 bot "Install fonts"
@@ -213,14 +123,6 @@ require_cask docker
 
 ## llvm with cland
 require_brew llvm
-
-action "link tmux conf"
-ln -s  $HOME/.dotfiles/tmux/.tmux.conf $HOME/.tmux.conf
-ok
-
-action "link .rgignore"
-ln -s  $HOME/.dotfiles/.rgignore $HOME/.rgignore
-ok
 
 action "Install tpm"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
@@ -276,10 +178,6 @@ npm i -g prettier
 npm i -g vscode-langservers-extracted
 npm i -g bash-language-server
 npm i -g vite
-
-bot "link clang-format"
-ln -s ~/.dotfiles/.clang-format ~/.clang-format
-
 
 # ###########################################################
 bot " Install Gui Applications"
---
+++ link/.zshenv
@@ -0,0 +1,37 @@
+# Enviroment variables
+
+export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_STATE_HOME="$HOME/.local/var"
+
+# Env
+export GOPATH="$XDG_DATA_HOME/go"
+export CARGO_HOME="$XDG_DATA_HOME/cargo"
+export RUSTUP_HOME="$XDG_DATA_HOME/rustup"
+
+# PATH
+[ -d "$HOME/.local/bin" ] && PATH="$HOME/.local/bin:$PATH"
+PathAppend() { [ -d "$1" ] && PATH="$PATH:$1"; }
+
+## Go
+PathAppend "$XDG_DATA_HOME/go/bin"
+PathAppend "/usr/local/go/bin"
+# Rust
+PathAppend "$CARGO_HOME/bin"
+# Lua
+PathAppend "$XDG_DATA_HOME/.luarocks/bin"
+unset PathAppend
+
+
+# Editor
+export EDITOR="nvim"
+export TERMINAL='kitty'
+
+# Bat
+export BAT_THEME="TwoDark"
+
+# Fzf
+export FZF_COMPLETION_TRIGGER='**'
+export FZF_DEFAULT_COMMAND='rg --files --hidden'
+export FZF_DEFAULT_OPTS='--height 90% --layout reverse --border --color "border:#b877db" --preview="bat --color=always {}"'
